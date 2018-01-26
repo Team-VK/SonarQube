0c0d5110f0337b5d74db5cda183991d846a896f8
Mika Aho <mika.aho@eficode.com>
2018-01-27 00:11:55 +0200

    Projectile creation

--- SonarQube/Assets/MoveSonar.cs
+++ SonarQube/Assets/MoveSonar.cs
@@ -6,17 +6,35 @@ public class MoveSonar : MonoBehaviour {
 
 	Light sonar;
 
-	// Use this for initialization
 	void Start() {
 		sonar = GameObject.Find("Sonar").GetComponent<Light>();
 	}
 	
-	// Update is called once per frame
 	void Update() {
 		Vector2 positionOnScreen = sonar.transform.position;
 		Vector2 mouseOnScreen = (Vector2)Input.mousePosition;
 		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-		sonar.transform.rotation =  Quaternion.Euler(new Vector3(angle,90,0f));
+		if(angle > 55) {
+			angle = 55;
+		}
+		else if(angle < -55) {
+			angle = -55;
+		}
+			
+		Quaternion rotation = Quaternion.Euler (new Vector3 (angle, 90, 0f));
+		sonar.transform.rotation = rotation;
+
+		if (Input.GetKeyDown ("space")) {
+			sendSonarWave(positionOnScreen, mouseOnScreen, rotation);
+		}
+	}
+
+	//Sends a sonar wave in the direction pointed by the sonar device
+	void sendSonarWave(Vector2 positionOnScreen, Vector2 mouseOnScreen, Quaternion rotation) {
+		GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		cylinder.transform.position = new Vector3 (positionOnScreen.x, positionOnScreen.y, 0);
+		cylinder.transform.localScale = new Vector3(1, 1, 1);
+		cylinder.transform.rotation = rotation;
 	}
 
 	float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
