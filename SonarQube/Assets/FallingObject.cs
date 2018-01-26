32442c2a5b1efb2e755e9b6801759b218ceec6dd
Taneli Vallo <taneli.vallo@eficode.com>
2018-01-27 00:36:03 +0200

    falling objects

---
+++ SonarQube/Assets/FallingObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.1f * Time.deltaTime, 0f );
+	}
+}
