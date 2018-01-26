e27daca79eadb4103fd294d05e4af50ba8a54553
Taneli Vallo <taneli.vallo@eficode.com>
2018-01-27 00:14:55 +0200

    merge

--- SonarQube/Assets/ProjectileBehavior.cs
+++ SonarQube/Assets/ProjectileBehavior.cs
@@ -14,13 +14,13 @@ public class ProjectileBehavior : MonoBehaviour {
 		
 	}
 
-	void OnCollisionEnter(Collision collision)
-	{
-		foreach (ContactPoint contact in collision.contacts)
-		{
-			Debug.DrawRay(contact.point, contact.normal, Color.white);
-		}
-		if (collision.relativeVelocity.magnitude > 2)
-			audioSource.Play();
-	}
+//	void OnCollisionEnter(Collision collision)
+//	{
+//		foreach (ContactPoint contact in collision.contacts)
+//		{
+//			Debug.DrawRay(contact.point, contact.normal, Color.white);
+//		}
+//		if (collision.relativeVelocity.magnitude > 2)
+//			audioSource.Play();
+//	}
 }
