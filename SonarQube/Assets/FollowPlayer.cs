bc1231cf181b2dec699bf0dd8a80a6e23eb9b765
Tomi Koskipahta <tkoskipahta@gmail.com>
2018-01-27 13:51:00 +0200

    Forgot to save scene, saved scene with submarine

---
+++ SonarQube/Assets/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var speed = 2f;
+
+		var sub_pos = GameObject.FindGameObjectWithTag("subumarine").transform.position;
+		var mine_pos = this.transform.position;
+
+		// example sub_pos.x
+		// example sub_pos.y
+
+		var direction = sub_pos - mine_pos;	
+	}
+}
