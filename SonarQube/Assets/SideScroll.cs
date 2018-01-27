309b810e2be5f21aa5f90d38ac5f1b06afcca3a2
Tomi Koskipahta <tkoskipahta@gmail.com>
2018-01-27 15:53:05 +0200

    Initial commit of side scrolling

---
+++ SonarQube/Assets/SideScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScroll : MonoBehaviour {
+
+	public float sideScrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Increase players x with parameterized side scrolling speed
+		var sub_pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+		var sub_pos.x += sideScrollSpeed * Time.deltaTime;
+		GameObject.FindGameObjectWithTag("Player").transform.position = sub_pos;
+	}
+}
