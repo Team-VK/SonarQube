403d295cb3f332c82d1caf37c4516efb7f55cf35
Mika Aho <mika.aho@eficode.com>
2018-01-28 13:41:39 +0200

    Mine behaviour development

---
+++ SonarQube/Assets/Code/MineBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		Debug.Log ("MINE BEHAVIOR COLLISION");
+		if(col.gameObject.name == "PushProjectile")
+		{
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log ("MINE BEHAVIOR COLLIDER");
+		if(col.gameObject.name == "PushProjectile")
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
